0841aae269ae731abd9605dea86c183a7dcec05c
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-17 20:33:11 -0300

    Add files via upload

---
+++ Lista1/08.py
@@ -0,0 +1,10 @@
+"""Digite as notas de um aluno para os 4 bimestres do ano e calcule a sua média ponderada. Os pesos para cada
+bimestre são, respectivamente: 2, 3, 4, 6."""
+
+nota1 = float(input('Nota 1:'))
+nota2 = float(input('Nota 2:'))
+nota3 = float(input('Nota 3:'))
+nota4 = float(input('Nota 4:'))
+
+media_ponderada = (nota1 * 2 + nota2 * 3 + nota3 * 4 + nota4 * 6) / 15
+print(f'Média ponderada = {media_ponderada:.2f}')
---
+++ Lista1/11.py
@@ -0,0 +1,10 @@
+"""Uma companhia de carros paga a seus empregados um salário de R$ 500,00 por mês mais uma comissão de R$ 50,00
+para cada carro vendido e mais 5% do valor da venda. Elabore um algoritmo para calcular e imprimir o salário do vendedor
+num dado mês recebendo como dados de entrada o nome do vendedor, o número de carros vendidos e o valor total das
+vendas."""
+
+nome = input('Nome do vendedor:')
+carros = int(input('Carros vendidos:'))
+vendas = float(input('Valor total das vendas:'))
+ns = 500 + (carros * 50) + (5 / 100 * vendas)
+print(f'O salário de {nome} é de R${ns:.2f}')
