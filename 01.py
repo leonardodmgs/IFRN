346bed422a8f72eb3156b1aa8e84b972c6817d2f
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-17 20:33:31 -0300

    Delete 01.py

--- 01.py
+++
@@ -1,6 +0,0 @@
-"""Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit.
-A fórmula de conversão é F ← (9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius."""
-
-celsius = float(input('Temperatura em ºC:'))
-fahrenheit = (9 * celsius + 160) / 5
-print(f'Temperatura em ºC = {celsius:.2f}\nTemperatura em Fahrenheit = {fahrenheit:.2f}')
