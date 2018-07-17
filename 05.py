ed2c4b6487c0befb52c38329bf185526cbfa00eb
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-17 20:37:02 -0300

    Delete 05.py

--- 05.py
+++
@@ -1,7 +0,0 @@
-"""Ler o valor correspondente ao salário mensal (variável SM) de um trabalhador e também o valor do
-percentual de reajuste (variável PR) a ser atribuído. Apresentar o valor do novo salário (variável NS)."""
-
-SM = float(input('Salário mensal R$:'))
-PR = float(input('Reajuste %:'))
-NS = SM + (PR / 100 * SM)
-print(f'O novo salário é de R${NS:.2f}')
