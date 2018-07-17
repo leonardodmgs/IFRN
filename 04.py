6b38ddb49813514743705f007f9c6a6f9b480ebf
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-17 20:35:27 -0300

    Delete 04.py

--- 04.py
+++
@@ -1,9 +0,0 @@
-"""4) Elaborar um programa que calcule e apresente o volume de uma caixa retangular, por meio da fórmula
-VOLUME ← COMPRIMENTO * LARGURA * ALTURA."""
-
-comprimento = float(input('Comprimento:'))
-largura = float(input('Largura:'))
-altura = float(input('Altura:'))
-volume = comprimento * largura * altura
-
-print(f'Volume da caixa = {volume:.2f}m³')
