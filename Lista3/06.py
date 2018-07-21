e11d3f247c2b37fb943891814a53cabf2d0448e6
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-21 11:10:27 -0300

    Create 06.py

---
+++ Lista3/06.py
@@ -0,0 +1,13 @@
+'''6) Escreva um programa que pergunta o valor de depósito inicial para uma poupança, e a taxa de rendimento mensal
+ Apresente o saldo dos próximos 24 meses, considerando o rendimento sobre o saldo atual de cada mês.'''
+
+deposito_inicial = float(input('Depósito inicial:'))
+taxa_rendimento = float(input('Taxa de rendimento %:'))
+
+mes = 1
+saldo = deposito_inicial
+
+while mes <= 24:
+    saldo = saldo + (saldo * taxa_rendimento/100)
+    print(f'Saldo no mês {mes} foi de R${saldo:.2f}')
+    mes = mes + 1
