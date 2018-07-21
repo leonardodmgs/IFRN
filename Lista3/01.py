c974c326c2a6ffdf9a8bacb44705c240f43ddbac
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-21 11:07:41 -0300

    Create 01.py

---
+++ Lista3/01.py
@@ -0,0 +1,11 @@
+'''1  Crie  um  programa  que  imprime  a  tabuada  de  um  número  digitado  pelo  usuário.  Ex.  Usuário
+digitou "2", o programa imprime: 2 x 1 = 2, 2 x 2 = 4, 2 x 3 = 6 ...'''
+
+n = int(input('Digite o número desejado:'))
+x = 1
+print(f'Imprimindo a tabuada do número {n}:')
+while x <= 10:
+
+    print(f'{n} x {x} = {n*x}')
+    x = x + 1
+    
