7e656dc8b541956bbcdbc66b4990d7557997f3cf
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-21 11:09:24 -0300

    Create 04.py

---
+++ Lista3/04.py
@@ -0,0 +1,13 @@
+
+''''4  Digite as notas de uma prova para uma turma de 10 alunos e calcule a média da turma. Dica
+: use uma variável para acumular a soma de todas as notas, por último, já fora do laço, faça a divisão para calcular a média'''
+
+x = 1
+soma = 0
+while x <= 10:
+    nota = float(input(f'Digite a {x} nota:'))
+    soma = soma + nota
+    x = x + 1
+x = x - 1  # remove um valor de x, caso contrário, a divisão ficaria incorreta.
+media = soma/x
+print(f'A média para as 10 notas é igual a {media:.2f}')
