a6492e95e0f848a6588d0bd99b36f2e547123f91
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-21 11:08:47 -0300

    Create 03.py

---
+++ Lista3/03.py
@@ -0,0 +1,11 @@
+'''3 Crie um programa em que o usuário digite 10 números e o programa apresente a soma desses números. Dica: use uma variável para
+acumular a soma dos números, como no exemplo: soma = soma + numero'''
+
+x = 1
+soma = 0
+
+while x <= 10:
+    numero = int(input(f'Digite o {x} número:'))
+    soma = soma + numero
+    x = x + 1
+print(f'A soma dos números é igual a {soma}')
