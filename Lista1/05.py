b3b12b6fb5acb1147125b10de065917ccd08f52a
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-23 12:38:55 -0300

    Lista 1

--- Lista1/05.py
+++ Lista1/05.py
@@ -1,5 +1,5 @@
 """Ler o valor correspondente ao salário mensal (variável SM) de um trabalhador e também o valor do
-percentual de reajuste (variável PR) a ser atribuído. Apresentar o valor do novo salário (variável NS)."""
+percentual de reajuste (variável PR) a ser atribuído. Apresentar o valor do novo salário (variável NS).""" 
 
 SM = float(input('Salário mensal R$:'))
 PR = float(input('Reajuste %:'))
