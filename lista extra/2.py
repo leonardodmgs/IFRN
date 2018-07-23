4294b39994f8bcb5881ed05f9dbbd73aa3ee0229
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-23 12:13:12 -0300

    lista extra

---
+++ lista extra/2.py
@@ -0,0 +1,15 @@
+# Questão 2
+'''Crie um programa que solicite a digitação de dois números e mostre o resultado das 4 operações'''
+n1 = int(input('Digite o primeiro número:'))
+n2 = int(input('Digite o segundo número:'))
+print(f'Soma = {n1 + n2}\nSubtração = {n1 - n2}\nMultiplicação = {n1 * n2}\nDivisão = {n1 / n2}')
+
+# ou
+
+n1 = int(input('Digite o primeiro número:'))
+n2 = int(input('Digite o segundo número:'))
+soma = n1 + n2
+subtração = n1 - n2
+multiplicação = n1 * n2
+divisão = n1 / n2
+print(f'Soma = {soma}\nSubtração = {subtração}\nMultiplicação = {multiplicação}\nDivisão = {divisão}')
