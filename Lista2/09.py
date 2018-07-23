11ce572344508e8448d0665fb5a9d7af781c375c
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-23 12:33:32 -0300

    Lista 2

--- Lista2/09.py
+++ Lista2/09.py
@@ -1,5 +1,4 @@
-"""Uma empresa paga R$ 12,50 por hora trabalhada (até 40 horas) e 18,50 por hora extra.
-Leia o número de horas semanais de um funcionário e informe o valor do pagamento da semana."""
+"""Uma empresa paga R$ 12,50 por hora trabalhada (até 40 horas) e 18,50 por hora extra. Leia o número de horas semanais de um funcionário e informe o valor do pagamento da semana."""
 
 horas = int(input('Horas semanal trabalhadas:'))
 if horas <= 40:
