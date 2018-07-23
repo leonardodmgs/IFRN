b89b4565330d5e51ee3d808fb7e644a9443b0af2
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-23 12:43:25 -0300

    Lista 3

--- Lista3/03.py
+++ Lista3/03.py
@@ -1,5 +1,5 @@
 '''3 Crie um programa em que o usuário digite 10 números e o programa apresente a soma desses números. Dica: use uma variável para
-acumular a soma dos números, como no exemplo: soma = soma + numero'''
+acumular a soma dos números, como no exemplo: soma = soma + numero''' 
 
 x = 1
 soma = 0
