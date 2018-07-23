9d03bcf99714642375e29d6eff0cb02b73d1cd84
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-23 12:19:42 -0300

    Lista 3

--- Lista3/04.py
+++ Lista3/04.py
@@ -1,4 +1,3 @@
-
 ''''4  Digite as notas de uma prova para uma turma de 10 alunos e calcule a média da turma. Dica
 : use uma variável para acumular a soma de todas as notas, por último, já fora do laço, faça a divisão para calcular a média'''
 
