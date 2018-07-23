792561a55bf4040afc42db476b0dd322cbd9176d
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-23 12:43:49 -0300

    Lista 3

--- Lista3/05.py
+++ Lista3/05.py
@@ -1,7 +1,7 @@
 '''5) Faça um programa que calcula a potência a partir de dois
 valores digitados pelo usuário. Não use o operador de potência,
 faça apenas multiplicações. Se o usuário digitar: base = 2, expoente = 3,
-o programa deve repetir a multiplicação 2 x 2 x 2 para obter o resultado.'''
+o programa deve repetir a multiplicação 2 x 2 x 2 para obter o resultado.''' 
 
 base = int(input('Digite a base:'))
 expoente = int(input('Digite o expoente:'))
