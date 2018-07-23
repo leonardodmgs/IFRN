ae1548db4191988ef9ad7ca7d24e95be8991a2ef
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-23 12:37:52 -0300

    Lista 1

--- Lista1/08.py
+++ Lista1/08.py
@@ -1,5 +1,5 @@
 """Digite as notas de um aluno para os 4 bimestres do ano e calcule a sua média ponderada. Os pesos para cada
-bimestre são, respectivamente: 2, 3, 4, 6."""
+bimestre são, respectivamente: 2, 3, 4, 6.""" 
 
 nota1 = float(input('Nota 1:'))
 nota2 = float(input('Nota 2:'))
