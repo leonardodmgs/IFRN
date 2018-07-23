4ed2bc03a5fe699397b5e2c987e4dd5781722195
Leonardo Domingos <40924451+leonardodmgs@users.noreply.github.com>
2018-07-23 12:37:30 -0300

    Lista 1

--- Lista1/09.py
+++ Lista1/09.py
@@ -1,6 +1,6 @@
 """O custo ao consumidor de um carro novo é a soma do custo de fábrica com a porcentagem do distribuidor e dos
 impostos, ambos aplicados ao custo de fábrica. Supondo que a porcentagem do distribuidor seja de 12% e a dos impostos
-de 45%, prepare um algoritmo para ler o custo de fábrica do carro e imprimir o custo ao consumidor."""
+de 45%, prepare um algoritmo para ler o custo de fábrica do carro e imprimir o custo ao consumidor.""" 
 
 custo_fabrica = float(input('Custo de fábrica R$:'))
 distribuidor = (12 / 100) * custo_fabrica
